f667666c47058607e415c76f4012b9c4af563f40
smm-gastov <77201169+smm-gastov@users.noreply.github.com>
2021-12-26 16:58:40 +0300

    Updates

---
+++ precache-manifest.be6c5f2d3b8ab695d845e75eb096b043.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07aeaac66432118e1954e40d9f5c3ed6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4af0be91eedf646e4c49",
+    "url": "./static/css/2.cdee455b.chunk.css"
+  },
+  {
+    "revision": "2ffa9a82f8a12bc6b705",
+    "url": "./static/css/main.d9c443e2.chunk.css"
+  },
+  {
+    "revision": "4af0be91eedf646e4c49",
+    "url": "./static/js/2.ee42604d.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "./static/js/2.ee42604d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ffa9a82f8a12bc6b705",
+    "url": "./static/js/main.d28bdc58.chunk.js"
+  },
+  {
+    "revision": "fb54b6bed906a3e09b6a",
+    "url": "./static/js/runtime-main.54d55149.js"
+  },
+  {
+    "revision": "4454c0d397a0900ca052283417be0bd5",
+    "url": "./static/media/kar1.4454c0d3.jpg"
+  },
+  {
+    "revision": "2006ca1b83a66699b3bfd37dc959908d",
+    "url": "./static/media/kar2.2006ca1b.jpg"
+  }
+]);
